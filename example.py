5b6166016bd4b14cfdcb5f08af44e65310347ecc
bradselph <Bselph512@gmail.com>
2024-10-28 02:27:05 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,22 +1,55 @@
 import harvester
+import logging
+from threading import Event
 
-# Simple example of using captcha harvester with one Harvester and printing captcha responses to console.
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def handle_captcha_response(response):
+    """Callback for handling solved captchas"""
+    logging.info(f"Got captcha response: {response['response'][:30]}...")
 
 def main():
     url = 'https://www.google.com/recaptcha/api2/demo'
-    # Scraping sitekey from url
+
+    logging.info("Fetching sitekey...")
     sitekey = harvester.Harvester.get_sitekey(url)
+    if not sitekey:
+        logging.error("Failed to get sitekey")
+        return
 
-    # Creating HarvesterManager object with additional argument response_callback which is function,
-    # that will be called everytime HarvesterManager pull captcha response from Harvseter ( after user solve captcha )
-    harvester_manager = harvester.HarvesterManger(response_callback=lambda x: print(x['response']))
-    # Adding Harvester object to HarvesterManager object with url and sitekey as arguments
-    harvester_manager.add_harvester(harvester.Harvester(url, sitekey))
-    # Launching all harvesters
-    harvester_manager.start_harvesters()
-    # Starting main_loop inside HarvesterManager object, that will manage all Harvesters
-    harvester_manager.main_loop()
+    logging.info(f"Found sitekey: {sitekey}")
 
+    manager = harvester.HarvesterManager(response_callback=handle_captcha_response)
+
+    manager.add_harvester(
+        harvester.Harvester(
+            url=url,
+            sitekey=sitekey
+        )
+    )
+
+    manager.add_harvester(
+        harvester.Harvester(
+            url=url,
+            sitekey=sitekey,
+            log_in=True,
+            open_youtube=True
+        )
+    )
+
+    logging.info("Starting harvesters...")
+    manager.start_harvesters()
+
+    try:
+        logging.info("Running main loop - Press Ctrl+C to stop")
+        manager.main_loop()
+    except KeyboardInterrupt:
+        logging.info("Shutting down...")
+    finally:
+        manager.stop()
 
 if __name__ == '__main__':
     main()
