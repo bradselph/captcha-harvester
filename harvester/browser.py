b99a5c45b23736004864fdf6fad9080ef054eb20
bradselph <Bselph512@gmail.com>
2024-10-28 02:40:37 -0400

    Update browser.py

--- harvester/browser.py
+++ harvester/browser.py
@@ -1,46 +1,46 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import os
 import logging
 
-DRIVER_CLOSED_MESSAGE = 'Unable to evaluate script: disconnected: not connected to DevTools\n'
-
-logging.getLogger('WDM').setLevel(logging.NOTSET)
-os.environ['WDM_LOG_LEVEL'] = '0'
-
-
 class Browser(Chrome):
     def __init__(self, executable: str = None, options: list = None, experimental_options: dict = None):
+        """
+        Initialize the browser
+
+        Args:
+            executable: Path to ChromeDriver executable
+            options: List of Chrome options
+            experimental_options: Dictionary of experimental options
+        """
         self.executable = executable
         if executable and not os.path.isfile(executable):
             self.executable = None
 
         self.options = ChromeOptions()
 
-        self.options.add_argument('--no-sandbox')
-        self.options.add_argument('--disable-dev-shm-usage')
-        self.options.add_argument('--disable-gpu')
-        self.options.add_argument('--disable-infobars')
-        self.options.add_argument('--disable-notifications')
-
+        # Add options
         if options:
             for option in options:
                 self.options.add_argument(option)
 
-        self.options.add_argument("--lang=en-US")
-
+        # Add experimental options
         if experimental_options:
-            for name, value in experimental_options.items():
-                self.options.add_experimental_option(name, value)
-
-        self.options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        self.options.add_experimental_option('useAutomationExtension', False)
+            if 'prefs' in experimental_options:
+                self.options.add_experimental_option('prefs', experimental_options['prefs'])
+            if 'excludeSwitches' in experimental_options:
+                self.options.add_experimental_option('excludeSwitches',
+                                                  experimental_options['excludeSwitches'])
+            if 'useAutomationExtension' in experimental_options:
+                self.options.add_experimental_option('useAutomationExtension',
+                                                  experimental_options['useAutomationExtension'])
 
     def start(self, url: str = None) -> None:
+        """Start the browser"""
         service = Service(self.executable) if self.executable else Service(ChromeDriverManager().install())
         super(Browser, self).__init__(service=service, options=self.options)
 
@@ -49,6 +49,7 @@ class Browser(Chrome):
             self.wait_for_ready_state()
 
     def wait_for_ready_state(self, timeout: int = 30) -> bool:
+        """Wait for page to be ready"""
         try:
             return WebDriverWait(self, timeout).until(
                 lambda driver: driver.execute_script('return document.readyState;') == 'complete'
@@ -80,7 +81,7 @@ class Browser(Chrome):
             return None
 
     def execute_script_safe(self, script: str, *args):
-        """Safe script execution with error handling"""
+        """Safely execute JavaScript"""
         try:
             return self.execute_script(script, *args)
         except Exception as e:
